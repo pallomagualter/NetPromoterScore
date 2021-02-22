64d4b00a9ec5abce295269b7e87aedc7d9fa4984
Palloma Gualter <pallomagualter@gmail.com>
2021-02-22 15:53:46 -0300

    mé´todos

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+/**
+ * GET => BUSCAR
+ * POST => SALVAR
+ * PUT => ALTERAR
+ * DELETE => DELETAR
+ * PATCH => ALTERAÇÃO ESPECÍFICA
+ */
+
 app.listen(3333, () => console.log("Server is running!"));
