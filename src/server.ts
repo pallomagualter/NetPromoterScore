aa2d817c8a9f447e56ad5a36a1cd139d6c76c683
Palloma Gualter <pallomagualter@gmail.com>
2021-02-22 15:58:16 -0300

    hello world

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
@@ -9,5 +9,9 @@ const app = express();
  * DELETE => DELETAR
  * PATCH => ALTERAÇÃO ESPECÍFICA
  */
+
+app.get("/users", (request, response) => {
+    return response.send("Hello World - NLW-04")
+});
 
 app.listen(3333, () => console.log("Server is running!"));
