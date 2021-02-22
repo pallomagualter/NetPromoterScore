af99616ed03f28b6b1527aae37e7221d1844e73b
Palloma Gualter <pallomagualter@gmail.com>
2021-02-22 16:06:37 -0300

    post

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,9 @@ app.get("/users", (request, response) => {
     return response.json( { message: "Hello World - NLW-04"} )
 });
 
+app.post("/", (request, response) => {
+    //assumindo que os dados foram recebidos e salvos e retornando mensagem para o usuário
+    return response.json({ message: "Dados salvos com sucesso! "})
+});
+
 app.listen(3333, () => console.log("Server is running!"));
